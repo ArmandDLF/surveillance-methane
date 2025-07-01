dc17d9b59c7a64ec8412a0981ab3fb79801ccc98
gabinsagot <gabin.sagot@etu.minesparis.psl.eu>
2025-07-01 14:32:03 +0200

    Update meteo.py
    The function now works, and allows the user to get the necessary meteo data, i.e. pressure and 10m-wind components around a (lat, long) point.
    A plotting function has been implemented via a boolean argument to allow visual representations.
    Interpolation to TROPOMI grid hasn't been implemented yet.

--- meteo.py
+++ meteo.py
@@ -3,6 +3,7 @@ import wxee
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+import scipy 
 
 # initialize ee API library
 ee.Initialize()
@@ -18,7 +19,7 @@ SATELLITES["GEOS"] = ['NASA/GEOS-CF/v1/rpl/tavg1hr', 27750, "PS", "U10M", "V10M"
 SATELLITES["GFS"] =["NOAA/GFS0P25", 27830, None, "u_component_of_wind_10m_above_ground", "v_component_of_wind_10m_above_ground"]
 
 
-def get_meteo(lat, long, date, time):
+def get_meteo(lat, long, date, time, plot=False):
 
 
     square = 250 #size of the square that will be studied in kilometers
@@ -46,13 +47,9 @@ def get_meteo(lat, long, date, time):
 
     data = []
 
-    #TEST WITH GEOS
 
-    name = 'NASA/GEOS-CF/v1/rpl/tavg1hr'
-    resolution = 27750
-    ind_pressure = 'PS'
-    ind_u, ind_v = 'U10M', 'V10M'
     """
+    ###TEST FOR ALL SATS
     for sat in SATS:
         
         name = SATELLITES[sat][0]
@@ -76,21 +73,58 @@ def get_meteo(lat, long, date, time):
         data.append(ds[ind_u])
         data.append(ds[ind_v])
     """
-    
+    #TEST WITH GEOS
+
+    name = 'NASA/GEOS-CF/v1/rpl/tavg1hr'
+    resolution = 27750
+    ind_pressure = 'PS'
+    ind_u, ind_v = 'U10M', 'V10M'
+
     ee_rect = ee.Geometry.Polygon(rectangle, None, False)
     ee_date = ee.Date(date)
 
     collection_filtered_sorted = (ee.ImageCollection(name)
                             .filterBounds(ee_rect)
                             .filterDate(ee_date)
-                            .select([resolution])).sort('system:time_start')
+                            .select([ind_pressure, ind_u, ind_v])).sort('system:time_start')
     
     ds = collection_filtered_sorted.wx.to_xarray(region=ee_rect, scale=resolution, crs='EPSG:4326', # crs = 4326 is lat,lon projection
                         masked=True, nodata=-999999)
     
-    ds_t = ds.isel(time=time)
 
-    return ds_t[ind_pressure, ind_u, ind_v] 
+    data = ds[[ind_pressure, ind_u, ind_v]]
 
-print(get_meteo(45, 0, '2025-06-10', '12'))
+    if plot:
+
+        fig = plt.figure()
+        ax = plt.subplot(1,3,1,projection=ccrs.PlateCarree())
+        ax.set_ylabel("Pressure (Pa)")
+        data[ind_pressure].plot(x='x',
+                    y='y',
+                    antialiased =False,
+                    transform=ccrs.PlateCarree())
+        
+        ax = plt.subplot(1, 3, 2, projection=ccrs.PlateCarree())
+        ax.set_ylabel("Eastward wind (m/s)")
+        data[ind_u].plot(x='x', 
+                            y='y', 
+                            antialiased=True, 
+                            transform=ccrs.PlateCarree())
+        
+        ax = plt.subplot(1,3,3,projection=ccrs.PlateCarree())
+        ax.set_ylabel("Northward wind (m/s)")
+        data[ind_v].plot(x='x', 
+                        y='y',
+                        antialiased=True,
+                        transform=ccrs.PlateCarree())
+        
+        plt.show()
+
+        data.
+
+    return data
+
+print(get_meteo(45, 5, "2025-06-10", 12))
+
+
 
