97093c4b4c19f29202c230a3b4a4cb5dc4f1801f
gabinsagot <gabin.sagot@etu.minesparis.psl.eu>
2025-07-02 10:23:29 +0200

    Update codepropre.py
    Copy-pasted the changes Eulalie couldn't push for some reason

--- codepropre.py
+++ codepropre.py
@@ -8,7 +8,7 @@ import cartopy.crs as ccrs
 
 import cartopy.feature as cfeature
 
-DATA_DIR = 'C:/Users/eulal/cours-info/tutorial_TROPOMI_Mines/work_data/official_product' 
+DATA_DIR = 'C:/Users/eulal/cours-info/tutorial_TROPOMI_Mines/work_data/official_product'
 jour = 13
 
 def distance(lat1d, lon1d, lat2d, lon2d):
@@ -29,7 +29,7 @@ def selection_carre(jour, temps, lat, lon):
     for i in range(len(files)):
         orbital=DATA_DIR + '/' + 'data' + '/' + str(jour) + '/' + files[i]
         ds = xr.open_dataset(orbital, group='PRODUCT')
-    
+   
         lat_max = min(lat + (taillecarre * 180 / (Rterre * np.pi)), 90)
         lat_min = max(lat - (taillecarre * 180 / (Rterre * np.pi)), -90)
 
@@ -49,7 +49,7 @@ def selection_carre(jour, temps, lat, lon):
 
     return liste
 
-donnees = selection_carre(13, 100000, 80.0, 50.0)
+'''donnees = selection_carre(13, 100000, 80.0, 50.0)
 
 def apply_log10(data):
     # let's be careful with zeros, and replace with NaNs
@@ -75,7 +75,74 @@ for i in range(len(donnees)):
                                 vmin=2,
                                 vmax=4)
 
+'''
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import os
+import numpy as np
 
+Rterre = 6378  # km
+taillecarre = 250  # km
+
+
+def selection_carre_optimal(jour, temps, lat, lon):
+
+    files=os.listdir(DATA_DIR + '/' + 'data' + '/' + str(jour))
+    tmax=0
+    i = 0
+    while temps > tmax:
+        tmax=int(files[i][45:51])
+        i += 1
+    orbital=DATA_DIR + '/' + 'data' + '/' + str(jour) + '/' + files[i+1]
+    ds=xr.open_dataset(orbital, group='PRODUCT')
+
+    lat_max = min(lat + (taillecarre * 180 / (Rterre * np.pi)), 90)
+    lat_min = max(lat - (taillecarre * 180 / (Rterre * np.pi)), -90)
+
+    lat_rad = lat * np.pi / 180
+    lon_delta = taillecarre * 180 / (Rterre * np.pi * np.cos(lat_rad))
+    lon_max = (lon + lon_delta + 180) % 360 - 180
+    lon_min = (lon - lon_delta + 180) % 360 - 180
+
+    mask = (
+        (ds['latitude'] >= lat_min) & (ds['latitude'] <= lat_max) &
+        (ds['longitude'] >= lon_min) & (ds['longitude'] <= lon_max)
+    )
+
+    # Vérifier qu'au moins un point est dans le masque
+    if mask.any().item():
+        carre = ds.where(mask, drop=True)
+    else:
+        carre = None
+
+    return ds, carre
+
+donnees = selection_carre_optimal(13, 100000, -25.6, 28.67)[1]
+
+def apply_log10(data):
+    # let's be careful with zeros, and replace with NaNs
+    buf = np.copy(data)
+    buf[buf==0] = np.nan
+    out = np.log10(buf)
+    return out
+
+# apply function and put in new 'log10_of_emissions' variable
+# HERE, we create a new variable in the dataset
+donnees['log10_of_emissions'] = xr.apply_ufunc(apply_log10, # first function name
+                                        donnees.methane_mixing_ratio_bias_corrected)
+
+fig = plt.figure(figsize = (7, 7))
+ax = plt.subplot(1,1,1,projection=ccrs.PlateCarree())
+donnees.log10_of_emissions.plot(x='longitude',
+                           y='latitude',
+                           antialiased=True,
+                           cmap='viridis',
+                           transform=ccrs.PlateCarree()
+                           )
+
+donnees
 
 ds=selection_carre(13, 50.0, 0.0)[0]
 carre=selection_carre(13, 50.0, 0.0)[1]
